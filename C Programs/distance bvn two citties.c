e3654b8c15ee7aedfa120607fd01bf8397799c5e
VIJAYWHAT <vijayrv042@gmail.com>
2023-08-12 23:12:28 +0530

    files added

--- C Programs/distance bvn two citties.c
+++ C Programs/distance bvn two citties.c
@@ -3,18 +3,18 @@
 int main (){
     float km,m,cm,ft,inch;
 
-    printf("enter the distancen in kilometers: ");
+    printf("enter the distance in kilometers: ");
 
     scanf("%f",&km);
     
-    m=km*1000;
-    cm=m * 100;
-    inch =cm/2.54;
+    m = km*1000;
+    cm = m * 100;
+    inch = cm/2.54;
     ft = inch/12;
 
-    printf("distance in meter= %f\n",m);
-    printf("distance in centimeter= %f\n",cm);
-    printf("distance in feet %f\n",ft);
+    printf("distance in meter = %f\n",m);
+    printf("distance in centimeter = %f\n",cm);
+    printf("distance in feet = %f\n",ft);
     printf("distance in inches = %f\n",inch);
 
     return 0;
