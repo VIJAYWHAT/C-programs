7ccc1a33617f5fc1b6865fd9cc7c5906aa60453c
VIJAYWHAT <89501520+VIJAYWHAT@users.noreply.github.com>
2023-08-11 23:22:48 -0700

    Updated StudentsMark.c

--- C Programs/StudentsMark.c
+++ C Programs/StudentsMark.c
@@ -11,9 +11,9 @@ char name[10][10];
 
 int main(){
 
-char sub[10][10] = {"Tamil","English","Maths","Science","Social"};
+char sub[5] = {"Tamil","English","Maths","Science","Social"};
 struct student s1;//object for students;
-int i,a,n,b;
+int i,a,n,b,k;
 char names[10][50];
 
 
@@ -24,45 +24,47 @@ int marks[n][5];
 int tot_mark[n];
 getchar();
 
+for(k = 0;k<n;k++){
+    printf("Enter student %d Name: ",k+1);
+    fgets(s1.name[k],MAX,stdin);
+    s1.name[k][strcspn(s1.name[k], "\n")];
+}
+
 for(i=0;i<n;i++)
 {
+
     tot_mark[i] = 0;
-    //printf("Enter student %d Name: ",i+1);
-    //fgets(s1.name[i],MAX,stdin);
-    //s1.name[i][strcspn(s1.name[i], "\n")] = '\0';
+
     printf("======================\n");
     printf("Enter student %d Marks:\n",i+1);
     printf("----------------------\n");
 
     for(int j = 0;j<5;j++)
     {
-        printf("Enter %s Mark:",sub[j]);
+        printf("Enter %s %s Mark:",s1.name[i],sub[j]);
+
         scanf("%d",&marks[i][j]);
         tot_mark[i] += marks[i][j];
     }
 
 }
 
-//int b;
-//for(b=0;b<5;b++){
-//tot_mark  += marks[b];
-//}
-//int avg;
-//avg = tot_mark /5;
-
 printf("=====================\n");
 printf("Students List: \n");
 printf("=====================\n");
 
 for(a=0;a<n;a++){
-
-    for(b=0;b<5;b++){
-       printf("student %d %s Mark:%d\n",a+1,sub[b],marks[a][b]);
+    printf("Student %d Name:%s",a+1,s1.name[a]);
+    printf("-------------------\n");
+    for(b=0;b<5;b++)
+    {
+        printf("%s Mark:%d\n",sub[b],marks[a][b]);
 
     }
     printf("-------------------\n");
     printf("Total Mark: %d/500\n",tot_mark[a]);
-    printf("-------------------\n");
+    printf("Precentage: %d%%\n",tot_mark[a]/5);
+    printf("===================\n");
 
 }
 return 1;
