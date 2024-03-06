4a9f962edffd16d5e0e1e275fc1ad724abeb168c
VijayWhat <vijayrv042@gmail.com>
2024-03-06 18:43:35 +0530

    files added

---
+++ C Programs/prime_number.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+void main()
+{
+int n,i = 1,k = 0;
+
+printf("\n Enter the any number: ");
+scanf("%d",&n);
+
+while(i <= n)
+{
+    if(n % i == 0)
+        k++;
+    i++;
+}
+if(k==2)
+    printf(" %d is a prime number",n);
+else
+    printf(" %d is a not prime number",n);
+
+}
