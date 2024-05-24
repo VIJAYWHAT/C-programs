0432ff15c274b9786805e89f0aa40cb85814b095
VijayWhat <vijayrv042@gmail.com>
2024-05-24 12:23:10 +0530

    changes in pyramid triangle

--- C Programs/pyramid_triangle.c
+++ C Programs/pyramid_triangle.c
@@ -1,19 +1,19 @@
-#include <stdio.h>
+#include <stdio.h> // Header file
 
-void main(){
+void main(){ // main function with no returns
 
-    int i,j,k,s,rows;
+    int i,j,k,s,rows; 
 
-    printf("Enter the Number of rows: ");
-    scanf("%d",&rows);
+    printf("Enter the Number of rows: "); // print function for display and getting input from the user
+    scanf("%d",&rows); // scan function for getting input from the user
 
-    for(i=1;i<=rows;i++){
-        for(j=1;j<=(rows-i);j++)
-            printf("  ");
+    for(i=1;i<=rows;i++){ // parent loop 
+        for(j=1;j<=(rows-i);j++) // child loop
+            printf("  "); // print space for triangle comes center
 
-        s = (i*2) - 1;
+        s = (i*2) - 1; // s to calculate the rows to print
 
-    for(k=1;k<=s;k++){
+    for(k=1;k<=s;k++){ // k loop for print the star
             printf(" *");
     }
     printf("\n\n");
