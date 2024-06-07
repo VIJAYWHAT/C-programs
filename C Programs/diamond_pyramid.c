48809b5581cbca8b0c65d68ecef60329a8a19e33
VijayWhat <vijayrv042@gmail.com>
2024-06-07 10:56:12 +0530

    commands added in diamond pyramid

--- C Programs/diamond_pyramid.c
+++ C Programs/diamond_pyramid.c
@@ -1,29 +1,41 @@
+// C program to print full 
+// diamond shape pyramid 
 #include <stdio.h> 
 
+// Prints diamond pattern 
 void printDiamond(int n) 
 { 
 	int space = n - 1; 
 
+	// Run loop till the number 
+	// of rows 
 	for (int i = 0; i < n; i++) 
 	{ 
-	
+		// Loop for the initially space 
+		// before the star printing 
 		for (int j = 0; j < space; j++) 
 			printf(" "); 
 
+		// Print the i+1 stars 
 		for (int j = 0; j <= i; j++) 
 			printf("* "); 
 
 		printf("\n"); 
-		space--;
+		space--; 
 	} 
 
+	// Repeat again in the reverse order 
 	space = 0; 
 
+	// run a loop till number of rows 
 	for (int i = n; i > 0; i--) 
 	{ 
+		// Loop for the initially space 
+		// before the star printing 
 		for (int j = 0; j < space; j++) 
 			printf(" "); 
 
+		// Print the i stars 
 		for (int j = 0; j < i; j++) 
 			printf("* "); 
 
@@ -32,6 +44,7 @@ void printDiamond(int n)
 	} 
 } 
 
+// Driver code 
 int main() 
 { 
 	printDiamond(8); 
