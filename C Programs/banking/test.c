ee3a34922c4983b92e089d088685b96da218ac61
VijayWhat <vijayrv042@gmail.com>
2024-07-05 18:42:50 +0530

    changes

--- C Programs/banking/test.c
+++ C Programs/banking/test.c
@@ -1,18 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
+#include <string.h> // Assuming banking_func.h contains the declaration for getCurrentDate()
 
+char*  getCurrentDate
 int main() {
-    time_t t;
-    struct tm *tmp;
-    char buffer[80];
+    char filename[55];
+    FILE *dp;
 
-    // Get the current time
-    time(&t);
-    tmp = localtime(&t);
+    strcpy(filename, "127101234.txt");
+    int amount = 500;
+    int balance = 1500;
 
-    // Format the date
-    strftime(buffer, sizeof(buffer), "%d-%m-%Y", tmp);
-    printf("Current date: %s\n", buffer);
+    dp = fopen(filename, "a"); // append the file
+    if (dp == NULL) {
+        perror("Error opening file");
+        return EXIT_FAILURE;
+    }
 
-    return 0;
+    fprintf(dp, "%s\tDeposit\tCredit\t%d\t%d\n", getCurrentDate(), amount, balance);
+
+    fclose(dp);
+
+    return EXIT_SUCCESS;
+}
+
+
+char* getCurrentDate() { // Function with return value and No Parameter
+    static char date[80];  // Static array to hold the date string
+    time_t t = time(NULL);
+    struct tm *tmp = localtime(&t);
+    
+    strftime(date, sizeof(date), "%d-%m-%Y", tmp);
+    return date;
 }
