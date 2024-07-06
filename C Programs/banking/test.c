aebfe1aad62cb51bfbebffc4ed5382271812d6e4
VijayWhat <vijayrv042@gmail.com>
2024-07-06 23:44:01 +0530

    print deposit template created

--- C Programs/banking/test.c
+++ C Programs/banking/test.c
@@ -3,7 +3,10 @@
 #include <time.h>
 #include <string.h> // Assuming banking_func.h contains the declaration for getCurrentDate()
 
-char*  getCurrentDate
+
+char*  getCurrentDate(); 
+void printStatment();
+
 int main() {
     char filename[55];
     FILE *dp;
@@ -18,13 +21,32 @@ int main() {
         return EXIT_FAILURE;
     }
 
-    fprintf(dp, "%s\tDeposit\tCredit\t%d\t%d\n", getCurrentDate(), amount, balance);
+    fprintf(dp, "%s\tDeposit\t\t    Credit\t\t%d  \t    %d\n", getCurrentDate(), amount, balance);
 
     fclose(dp);
-
+    printStatment();
     return EXIT_SUCCESS;
 }
 
+void printStatment()
+{   
+
+    system("cls");
+    char filename[55];
+    char statement[100];
+    
+    // sprintf(filename, "%d.txt", ac_no);
+
+    FILE *st = fopen("127101234.txt", "r"); // also we directly open the file in the FILE declaration
+    printf("\n");
+
+    if(st == NULL) perror("No statement Available");
+    
+    while(fgets(statement, sizeof(statement), st)){
+        printf("%s", statement);
+    }
+    
+}
 
 char* getCurrentDate() { // Function with return value and No Parameter
     static char date[80];  // Static array to hold the date string
