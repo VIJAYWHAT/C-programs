c6262588c2e0b02e456cdf3f71ab051c7cbf0418
Vijay Raja <89501520+VIJAYWHAT@users.noreply.github.com>
2024-07-14 21:24:06 +0530

    Update factorial of num.c

--- C Programs/factorial of num.c
+++ C Programs/factorial of num.c
@@ -4,18 +4,16 @@ int main(){
 
     int i,n,fact = 1;
 
-    printf("enter the number:");
+    printf("enter the number: ");
     scanf("%d",&n);
 
     for(i=1;i<=n;i++){
-
     fact = fact * i;
-
     }
     
     if(n < 0){ 
-        printf("number is negative");
+        printf("number is negative!");
     }
-    printf("factorial:%d \n",fact);
+    printf("factorial: %d \n",fact);
 
 }
